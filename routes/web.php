b1c14b5b0a0be5026c769fff8f3e8a0ff8df3a79
aikoextended <putrimeyliyaalfath@gmail.com>
2024-09-26 14:34:48 +0700

    Pesan commit kamu

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Putri Meyliya Rachmawati'], ['title' => 'About']);
+    return view('about', ['nama' => 'Putri Meyliya Rachmawati', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
